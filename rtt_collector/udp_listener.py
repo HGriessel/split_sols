d86234eda1417b947243dc380988cc3bb647cc84
Hgriessel <hgriessel2@gmail.com>
2023-05-30 19:36:30 +0000

    dockerised udpserver

--- rtt_collector/udp_listener.py
+++ rtt_collector/udp_listener.py
@@ -1,28 +1,21 @@
 import socket
 import time
 import random
-# Create a UDP socket
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Bind the socket to localhost and port 161
-server_address = ('localhost', 161)
+server_address = ('0.0.0.0', 161)
 server_socket.bind(server_address)
 
-# Receive and process incoming packets
 while True:
     print('Waiting for incoming packets...')
     data, address = server_socket.recvfrom(1024)  # Adjust buffer size as per your requirements
 
-    # Process the received packet
     print('Received packet from:', address)
     print('Data:', data.decode())
     
-    time.sleep(random.uniform(0.3, 1))
-    # Generate the response string
+    time.sleep(random.uniform(0.5, 1))
     response = 'Hello, client!'
 
-    # Send the response back to the client
     server_socket.sendto(response.encode(), address)
 
-# Close the socket
-# server_socket.close()
